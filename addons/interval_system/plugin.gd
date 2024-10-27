20beb8969e49fe00ab2f0bb77762284b16ccb39c
Eric Sexton <foopis23@gmail.com>
2024-10-27 12:04:16 -0400

    proper plugin structure

--- addons/interval_system/plugin.gd
+++ addons/interval_system/plugin.gd
@@ -3,9 +3,8 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_autoload_singleton("IntervalSystem", "addons/interval_system/interval_system.gd")
+	add_autoload_singleton("IntervalSystem", "res://addons/interval_system/interval_system.gd")
 
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("IntervalSystem")
-
